e8c5a53f1a5431576c5acb8e162ee19d06c49661
youssef ghoubach <ghoubach.youssef@gmail.com>
2014-07-12 16:12:29 +0000

    clear

--- Transportation-mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/youssef/NetBeansProjects/Transportation/Transportation-mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/youssef/NetBeansProjects/Transportation/Transportation-mobile/platforms/android/CordovaLib/AndroidManifest.xml \
--- Transportation-mobile/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/youssef/NetBeansProjects/Transportation/Transportation-mobile/platforms/android/ant-build/AndroidManifest.xml : \
-/home/youssef/NetBeansProjects/Transportation/Transportation-mobile/platforms/android/AndroidManifest.xml \
